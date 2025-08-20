04452912af4bba250332b5a629b84eaadb549e0d
Abdullah <abdullahrehman1019@gmail.com>
2025-08-20 15:42:21 -0600

    multiple selection done

--- lib/multiple_selection.dart
+++ lib/multiple_selection.dart
@@ -12,30 +12,32 @@ class _MultipleSelectionDemoState extends State<MultipleSelectionDemo> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Multi Selection"),),
-      body: ListView.builder(
-        itemCount: 7,
-        itemBuilder: (context, i){
-         return ListTile(
-           onTap: (){
-             if(selectedIndex.contains(i)){
-               selectedIndex.remove(i);
-             }
-             else{
-               selectedIndex.add(i);
-             }
-             setState(() {});
-           },
-           tileColor: selectedIndex.contains(i) ? Colors.blue : Colors.white70,
-           textColor: selectedIndex.contains(i) ? Colors.white : Colors.black,
-           iconColor: selectedIndex.contains(i) ? Colors.white : Colors.black,
-            leading: Icon(Icons.person),
-            title: Text("Multiple Selection, $selectedIndex"),
-           subtitle: Text("$i"),
-            trailing: Icon(Icons.arrow_forward_ios_sharp),
-          );
-        }
+      appBar: AppBar(
+        title: Text("Multiple Selection"),
+        backgroundColor: Colors.blue,
       ),
+      body: ListView.builder(
+          itemCount: 6,
+          itemBuilder: (context, i){
+            return ListTile(
+              onTap: (){
+                if(selectedIndex.contains(i)){
+                  selectedIndex.remove(i);
+                }
+                else{
+                  selectedIndex.add(i);
+                }
+                setState(() {});
+              },
+              tileColor: selectedIndex.contains(i) ? Colors.blue : Colors.white,
+              textColor: selectedIndex.contains(i) ? Colors.white : Colors.black,
+              iconColor: selectedIndex.contains(i) ? Colors.white : Colors.black,
+              leading: Icon(Icons.add_ic_call_outlined),
+              title: Text("Multiple Selection, $selectedIndex"),
+              subtitle: Text("Index, $i"),
+              trailing: Icon(selectedIndex.contains(i) ? Icons.arrow_downward_sharp : Icons.arrow_forward_ios),
+            );
+          }),
     );
   }
 }
