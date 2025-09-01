6bce60c31ce61173958b7122ffd09e5d8de0b063
Abdullah <abdullahrehman1019@gmail.com>
2025-09-01 16:05:14 -0600

    Bottom Bar done

---
+++ lib/curved_bar.dart
@@ -0,0 +1,37 @@
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_sec_5/listtiledemo.dart';
+import 'package:flutter_sec_5/login_page.dart';
+import 'package:flutter_sec_5/single_selection.dart';
+
+class CurvedBarDemo extends StatefulWidget {
+  const CurvedBarDemo({super.key});
+
+  @override
+  State<CurvedBarDemo> createState() => _CurvedBarDemoState();
+}
+
+class _CurvedBarDemoState extends State<CurvedBarDemo> {
+  int selectedIndex = 0;
+  List<Widget> screenList = [LoginPage(), SingleSelectionDemo(), ListTileDemo()];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: screenList.elementAt(selectedIndex),
+      bottomNavigationBar: CurvedNavigationBar(
+        backgroundColor: Colors.blueAccent.shade100,
+        color: Colors.blue,
+        buttonBackgroundColor: Colors.lightBlue,
+        animationDuration: Duration(milliseconds: 300),
+        items: const [
+          Icon(Icons.home, size: 30, color: Colors.white),
+          Icon(Icons.favorite, size: 30, color: Colors.white),
+          Icon(Icons.person, size: 30, color: Colors.white),
+        ],
+        onTap: (index) => setState(() => selectedIndex = index),
+      ),
+
+
+    );
+  }
+}
